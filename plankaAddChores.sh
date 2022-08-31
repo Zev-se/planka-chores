2e0fabd7d3bb05ea38c109002d7a4aa5007d2a06
zev <git@bixt.se>
2022-08-31 20:20:43 +0200

    added code shell

--- plankaAddChores.sh
+++ plankaAddChores.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+# Function get token
+function getToken () {
+	echo "getToken"
+}
+
+# Function add card to board due date next week
+function addCardsNextWeek (){
+	echo "add Cards Next Week"
+}
+
+# Fuction next sunday date
+function getDateOfNextSunday () {
+	dateOfNextSunday=$(date +%d-%m-%Y -d "Next Sunday")
+	echo "return date of next Sunday"
+	#return $dateOfNextSunday
+}
+
+getToken
+getDateOfNextSunday
+echo "some text" + $dateOfNextSunday
