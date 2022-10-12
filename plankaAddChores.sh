c702601e68817aadb485e5ec421c4f6ca514d55e
zev <git@bixt.se>
2022-10-12 19:58:16 +0200

    Token function now working.

--- plankaAddChores.sh
+++ plankaAddChores.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 
+# Variables
+host=http://185.189.49.210:3000
+
 # Function get token
 function getToken () {
 	echo "getToken"
+	username=$(jq -j '.credentials.Username' variables.json)
+	password=$(jq -j '.credentials.Password' variables.json)
+#	echo $username
+#	echo $password
+	echo \"emailOrUsername=$username\&password=$password\"
+	curl -X POST --data emailOrUsername=$username\&password=$password $host/api/access-tokens > token.json
+	token=$(jq -j '.item' token.json)
+	echo $token
 }
 
 # Function add card to board due date next week
 function addCardsNextWeek (){
 	echo "add Cards Next Week"
+	nextWeek=$(date "+%V" )
+	echo $nextWeek
 }
 
 # Fuction next sunday date
 function getDateOfNextSunday () {
 	dateOfNextSunday=$(date +%d-%m-%Y -d "Next Sunday")
-	echo "return date of next Sunday"
+	#echo "return date of next Sunday"
 	#return $dateOfNextSunday
 }
 
 getToken
 getDateOfNextSunday
-echo "some text" + $dateOfNextSunday
+addCardsNextWeek
+#echo "some text" + $dateOfNextSunday
